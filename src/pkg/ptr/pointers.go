4e30ff65e21d240dd8714d09c5644d01ff4f63cd
Matt Mihic <matt.mihic@gmail.com>
2023-12-06 11:18:19 -0500

    Int64/Int32 pointer support

--- src/pkg/ptr/pointers.go
+++ src/pkg/ptr/pointers.go
@@ -13,6 +13,16 @@ func Int(n int) *int {
 	return &n
 }
 
+// Int64 returns an int64 pointer for the given value.
+func Int64(n int64) *int64 {
+	return &n
+}
+
+// Int32 returns an int32 pointer for the given value.
+func Int32(n int32) *int32 {
+	return &n
+}
+
 // Float64 returns a float64 pointer for the given value.
 func Float64(n float64) *float64 {
 	return &n
